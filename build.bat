6d4044457fc987486b9112a351bd8567a88f9122
Emre Kovanci <kovanciemre@gmail.com>
2023-12-14 16:33:55 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@
 
 call %~dp0\vars.bat
 
-call "%BUILD_BAT%" %PROJECT%Editor Win64 Development "%UPROJECT_PATH%" -WaitMutex -NoHotReload
+call "%BUILD_BAT%" %PROJECT%Editor Win64 Development "%UPROJECT_PATH%" -WaitMutex -FromMsBuild
