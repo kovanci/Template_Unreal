681ce9297740f2bd55486562d6d6f791a342fd55
Emre Kovanci <kovanciemre@gmail.com>
2024-01-18 11:05:20 +0300

    Update TP_ChimpGame.Build.cs

--- Source/TP_ChimpGame/TP_ChimpGame.Build.cs
+++ Source/TP_ChimpGame/TP_ChimpGame.Build.cs
@@ -31,7 +31,8 @@ public class TP_ChimpGame : ModuleRules
             "DeveloperSettings",
             "EnhancedInput",
             "UMG",
-            "CommonUI"
+            "CommonUI",
+            "GameplayMessageRuntime"
         });
     }
 }
