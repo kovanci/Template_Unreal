39a04ff36c23c42d2e8275cce489261c7cbe6993
Emre Kovanci <kovanciemre@gmail.com>
2024-04-08 23:05:38 +0300

    add package.bat

---
+++ package.bat
@@ -0,0 +1,29 @@
+@echo off
+
+call %~dp0\vars.bat
+
+call "%UAT_BAT%" ^
+-ScriptsForProject="%UPROJECT_PATH%" ^
+BuildCookRun ^
+-project="%UPROJECT_PATH%" ^
+-noP4 ^
+-clientconfig=Development ^
+-serverconfig=Development ^
+-nocompile ^
+-nocompileeditor ^
+-installed ^
+-unrealexe="%UE5EDITOR_CMD_EXE%" ^
+-utf8output ^
+-platform=Win64 ^
+-build ^
+-cook ^
+-map=Entry ^
+-CookCultures=en ^
+-unversionedcookedcontent ^
+-pak ^
+-compressed ^
+-SkipCookingEditorContent ^
+-stage ^
+-package ^
+-archive ^
+-archivedirectory="%PROJECT_DIR%/PackagedGame"
--- vars.bat
+++ vars.bat
@@ -11,3 +11,4 @@ set UE5_DIR=C:\EpicGames\UE_5.3
 set UE5EDITOR_EXE=%UE5_DIR%\Engine\Binaries\Win64\UnrealEditor.exe
 set UE5EDITOR_CMD_EXE=%UE5_DIR%\Engine\Binaries\Win64\UnrealEditor-Cmd.exe
 set BUILD_BAT=%UE5_DIR%\Engine\Build\BatchFiles\Build.bat
+set UAT_BAT=%UE5_DIR%\Engine\Build\BatchFiles\RunUAT.bat
