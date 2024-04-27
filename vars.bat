d4e0093bd3b92f87e515f3b49ab288078ab34a9f
Emre Kovanci <kovanciemre@gmail.com>
2024-04-27 18:28:05 +0300

    update ue path

--- vars.bat
+++ vars.bat
@@ -9,7 +9,7 @@ set PROJECT=TP_Chimp
 set PROJECT_DIR=%ROOTDIR%
 set UPROJECT_PATH=%PROJECT_DIR%\%PROJECT%.uproject
 
-set UE5_DIR=C:\EpicGames\UE_5.3
+set UE5_DIR=C:\EpicGames\UE_5.4
 set UE5EDITOR_EXE=%UE5_DIR%\Engine\Binaries\Win64\UnrealEditor.exe
 set UE5EDITOR_CMD_EXE=%UE5_DIR%\Engine\Binaries\Win64\UnrealEditor-Cmd.exe
 set BUILD_BAT=%UE5_DIR%\Engine\Build\BatchFiles\Build.bat
